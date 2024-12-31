e0ec062d08d66fdfc02fc7a0d6852147fed27298
Akiyoshi <zundokokiyoshi999@gmail.com>
2024-12-31 19:13:06 +0900

    add jest-unit

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,13 @@
 import type { Config } from "jest";
 
 const config: Config = {
+  reporters: [
+    "default",
+    [
+      "jest-junit",
+      { outputDirectory: "./reports", outputName: "rep-junit.xml" },
+    ],
+  ],
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
